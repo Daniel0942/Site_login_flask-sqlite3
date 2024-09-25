53dae3b666bafb20d7d36170e58a13574d635e1d
DanielBomfim22 <danielufc131@gmail.com>
2024-09-25 17:03:27 -0300

    banco de dados feito

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash
-from models.table import conexao, criarTabela
+from models.table import conexao, criarTabela_users, criarTabela_gerenciador
 import sqlite3
 
 app = Flask(__name__)
@@ -19,13 +19,30 @@ def cadastrar():
         cursor.execute("INSERT INTO users(username, password) VALUES(?, ?)", (username, password))
         conectar.commit()
         flash("Usuário cadastrado com sucesso !")
+        return render_template("produto.html")
     except sqlite3.IntegrityError:
         flash("[ERRO], Usuário já existe !")
     finally:
         conectar.close()
-
     return render_template("index.html")
 
+@app.route("/formulario", methods = ["POST"])
+def formulario():
+    produto = request.form["produto"]
+    valor = request.form["valor"]
+    quantidade = request.form["quantidade"]
+    try:
+        conectar = conexao()
+        cursor = conectar.cursor()
+        cursor.execute('INSERT INTO gerenciador (produto, valor, quantidade) VALUES (?, ?, ?)', (produto, valor, quantidade))
+        conectar.commit()
+        cursor.close()
+        flash("Produto cadastrado com sucesso")
+    except :
+        flash("[ERRO], Digite os dados novamente !")
+    return render_template("produto.html")
+
 if __name__ == "__main__":
-    criarTabela()
+    criarTabela_users()
+    criarTabela_gerenciador()
     app.run(debug=True)  # "debug" é pra atualizações automáticas
